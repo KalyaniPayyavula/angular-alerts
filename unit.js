5848c5aa8c5a11c9d0cd16560ee1029ccef0c27f
Drahomír Hanák <drahomir.hanak@gmail.com>
2014-01-04 13:34:07 +0100

    chore(alerts): rename to angular-alerts

--- unit.js
+++ unit.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
 		files: [
 			{pattern: 'bower_components/angular/angular.js', included: true},
 			{pattern: 'bower_components/angular-mocks/angular-mocks.js', included: true},
-			{pattern: 'Alerts.js', watched: true, included: true, served: true},
+			{pattern: 'angular-alerts.js', watched: true, included: true, served: true},
 			{pattern: 'AlertsSpec.js', watched: true, included: true, served: true}
 		],
 		plugins: [
